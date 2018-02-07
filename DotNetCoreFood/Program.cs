9c0a5b106d8922bdddb3a629bb5b04f11c2e1107
Brian Schroer <brianschroer@gmail.com>
2018-02-07 14:29:01 -0600

    Added configuration source

--- DotNetCoreFood/Program.cs
+++ DotNetCoreFood/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace DotNetCoreFood
 {
@@ -17,6 +10,19 @@ namespace DotNetCoreFood
             BuildWebHost(args).Run();
         }
 
+        /// <summary>
+        /// Default builder:
+        /// 1) uses Kestrel web server
+        /// 2) enables IIS integration
+        /// 3) sets up default logging to command line console / VS2017 Output window
+        /// 4) makes IConfiguration service available, using (whichever it finds first):
+        ///     - command line arguments
+        ///     - environment variables
+        ///     - user secrets
+        ///     - appsettings.json (create via Add | New Item | ASP.NET Configuration File)
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns><see cref="IWebHost"/> implementation.</returns>
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
--- DotNetCoreFood/Startup.cs
+++ DotNetCoreFood/Startup.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DotNetCoreFood
@@ -17,8 +14,17 @@ namespace DotNetCoreFood
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.' 
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        /// <param name="configuration">Not added by default. 
+        /// I added it manually. Dependency injection knows what to do with it.</param>
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env,
+            IConfiguration configuration)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +33,8 @@ namespace DotNetCoreFood
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                string greeting = configuration["Greeting"] ?? "Hello, World!";
+                await context.Response.WriteAsync(greeting);
             });
         }
     }
